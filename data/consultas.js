56f8767bbea9bd6adf303b433e8ffb5f13a56633
JPmarqs <joaop.marques026@gmail.com>
2024-06-06 21:02:28 -0300

    ajustes no endpoint e BD

--- data/consultas.js
+++ data/consultas.js
@@ -105,11 +105,11 @@ async function updateConsultas(id, consulta) {
         ' WHERE "IDCONSULTA" = $6';
 
     const values = [
-        consulta[0].IDPACIENTECON,
-        consulta[0].IDMEDICOCON,
-        consulta[0].DATACON,
-        consulta[0].SITUACAOCON,
-        consulta[0].OBSERVCON,
+        consulta.IDPACIENTECON,
+        consulta.IDMEDICOCON,
+        consulta.DATACON,
+        consulta.SITUACAOCON,
+        consulta.OBSERVCON,
         id,
     ]
 
